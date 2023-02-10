2703c6d76dd850d2406724bc93752eee2b836b62
OscarOrtiz <tippingtheiceberg@gmail.com>
2023-02-10 13:44:30 -0600

    add interavtive nav bar for mobile users

---
+++ script.js
@@ -0,0 +1,20 @@
+let hamburger = document.querySelector(".header__burger");
+let links = document.querySelector(".nav__links--inactive")
+let linksAfter = window.getComputedStyle(links,"::after");
+let burgerImg = document.querySelector(".burger__img")
+
+hamburger.addEventListener("click", ()=>{
+    if(links.className == "nav__links--inactive"){
+        links.className = "nav__links--active"
+        linksAfter.className = "nav__links--active::after";
+        burgerImg.style.opacity = ".4"
+    }
+    else{
+        links.className = "nav__links--inactive"
+        linksAfter.className = "nav__links--inactive::after";
+        burgerImg.style.opacity = "1"
+    }
+})
+
+
+console.log(links,linksAfter)
